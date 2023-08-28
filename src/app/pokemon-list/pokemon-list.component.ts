6a52e10c0d6495942623495151e4b3da91f594c8
lauannyrb <lauannyrodrigues05@gmail.com>
2023-08-28 15:59:14 -0300

    Add cards

---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.sass']
+})
+export class PokemonListComponent {
+  public pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbasaur',
+      types:[
+        Type.Grass,
+        Type.Poison,
+      ]    
+     }
+  ];
+  
+
+}
+
+interface Pokemon {
+  image: string;
+  number: number;
+  name: string;
+  types: Type[];
+}
+
+enum Type{
+  Grass = 'Grass',
+  Poison = 'Poison'
+}
